debf6c8d7cd941d8d7a4c22b5d44eb52bd9e6511
Yusuf-Islam-Tunbul <50180280+Yusuf-Islam-Tunbul@users.noreply.github.com>
2022-02-23 21:22:06 +0300

    Proje dosyası ekle.

---
+++ 20211213_CustomForeach/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿
+namespace _20211213_CustomForeach
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.run_button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // run_button
+            // 
+            this.run_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.run_button.ForeColor = System.Drawing.Color.Red;
+            this.run_button.Location = new System.Drawing.Point(296, 145);
+            this.run_button.Name = "run_button";
+            this.run_button.Size = new System.Drawing.Size(142, 81);
+            this.run_button.TabIndex = 0;
+            this.run_button.Text = "RUN";
+            this.run_button.UseVisualStyleBackColor = true;
+            this.run_button.Click += new System.EventHandler(this.run_button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.run_button);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button run_button;
+    }
+}
+
---
+++ 20211213_CustomForeach/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _20211213_CustomForeach
+{
+    public partial class Form1 : Form
+    {
+        List<string> cities = new List<string>();
+        int[] numbers = { 1, 2, 3 };
+        public Form1()
+        {
+            InitializeComponent();
+            CreateList();
+        }
+        void CreateList()
+        {
+            cities.Add("Çankırı");
+            cities.Add("Burdur");
+            cities.Add("Of");
+        }
+
+        private void run_button_Click(object sender, EventArgs e)
+        {
+            CustomForeachMethod(cities);
+        }
+
+        void CustomForeachMethod(List<string> data)
+        {
+            List<string> copied_data = data.ToList();
+
+            while (copied_data.Count != 0)
+            {
+                MessageBox.Show(copied_data[0]);
+                copied_data.RemoveAt(0);
+            }
+        }
+    }
+}
